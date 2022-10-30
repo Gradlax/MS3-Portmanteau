a3b44d9f2bbc284943844a951c9ca44903ee1146
Gradlax <valhama@protonmail.com>
2022-10-30 11:46:30 -0500

    Another one of these commits, (works css)

--- Portmanteau/react-frontend/src/container/Work/Work.jsx
+++ Portmanteau/react-frontend/src/container/Work/Work.jsx
@@ -56,12 +56,46 @@ useEffect(() => {
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
+              <motion.div 
+              whileHover={{opacity: [0, 1]}}
+              transition={{duration: 0.4, ease: 'easeInOut', staggerChildren: 0.5 }}
+              className="app__work-hover app__flex">
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+            <motion.div 
+            whileInView={{scale: [0, 1]}}
+              whileHover={{scale: [1, 0.9]}}
+              transition={{duration: 0.3}}
+              className="app__flex"
+              >
+                <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
             </div>
 
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
           </div>
         ))}
       </motion.div>
-  )
-}
+    </>
+  );
+};
 
-export default Work
+export default AppWrap(Work, 'work');
